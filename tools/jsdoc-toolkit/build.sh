ceb47356b8aa41b8fb94a58a3fd222d6227f11bf
linlingyu <linlingyu@baidu.com>
2011-07-25 21:47:14 +0800

    CODESEARCH功能 [Feature] codesearch功能接口化 tools：增加codesearch

--- tools/jsdoc-toolkit/build.sh
+++ tools/jsdoc-toolkit/build.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../www/js mycode/tangram-base.js
 java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../www/js mycode/tangram-component.js
