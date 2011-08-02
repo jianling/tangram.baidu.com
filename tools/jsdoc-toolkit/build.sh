ab9df59567f2d14cab3d22893e8461fca43306e3
linlingyu <linlingyu@baidu.com>
2011-08-02 14:28:54 +0800

    更改目录

--- tools/jsdoc-toolkit/build.sh
+++ tools/jsdoc-toolkit/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../www/js mycode/empty.js
-java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../www/js mycode/tangram-base.js
-java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../www/js mycode/tangram-component.js
+java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../src/js mycode/empty.js
+java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../src/js mycode/tangram-base.js
+java -jar jsrun.jar app/run.js -a -t=templates/tangram/ -d=../../src/js mycode/tangram-component.js
