44a6730cea72f28bc7a2b6cf5a0f4526b4561722
chengyang <zhaochengyang@baidu.com>
2011-08-11 19:01:08 +0800

    ﻿提交demo

--- src/demo/baidu_swf_Proxy/conf.js
+++ src/demo/baidu_swf_Proxy/conf.js
@@ -7,35 +7,28 @@ var conf = {
     demoType: [{key: 'default', val: 'baidu.swf.create'}],
     'default': {
         pageConf: {
-            html: '<div id="resultArea"><object width="200" height="150" align="middle" id="flash" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"><param value="./baidu_swf_create/test.swf" name="movie"><embed width="200" height="150" align="middle" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="flash" src="./baidu_swf_create/test.swf"></object></div>'
-        },
-        dateInput1: {
-        	label: '宽：',
-            type: 'text',
-            defaultValue: "300",
-            size: 5
-        },
-        dateInput2: {
-        	label: '高：',
-            type: 'text',
-            defaultValue: "200",
-            size: 5
+            html: '<div id="resultArea"><object width="100%" height="160" align="middle" id="test" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"><param value="transparent" name="wmode"><param value="always" name="allowscriptaccess"><param value="../demo/base/line.swf" name="movie"><embed width="100%" height="160" align="middle" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="test" src="./baidu_swf_Proxy/line.swf" allowscriptaccess="always" ver="9.0.0" errormessage="载入FLASH出错" wmode="transparent"></object></div>'
         },
         btn1: {
             type: 'button',
-            defaultValue: '设置影片尺寸',
-            depend: ['dateInput1', 'dateInput2'],
+            defaultValue: '给Flash传递数据',
             event: {
             	eventName: 'onclick',
-            	handler: function(arg0, arg1){
-            		var movie = baidu.swf.getMovie("flash");
-            		movie.width = arg0;
-            		movie.height = arg1;
+            	handler: function(){
+            		function flashLoaded() {   
+					    baidu.ajax.get('./baidu_swf_Proxy/two_line.json', function(xhr){
+					        var data = baidu.json.decode(xhr.responseText);
+					        if (data) {
+					          proxy.call("setFlashLineData", data, 1);
+					        }
+					      });  
+					}
+					var proxy = new T.swf.Proxy("test", "setFlashLineData", flashLoaded);
             	}
             }
         }
     },
     groups: {
-        'default': [['dateInput1', 'dateInput2'], ['btn1']]
+        'default': [['btn1']]
     }
 };  
