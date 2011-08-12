a5efd027bd5796a51467657033a0985b7ddb6fab
aranwong <cnwrr@126.com>
2011-08-12 16:58:09 +0800

    second

---
+++ src/demo/baidu_dom_getAncestorByClass/conf.js
@@ -0,0 +1,29 @@
+var conf = {
+    clazz: {
+        type: 'method',
+        'method': 'baidu.dom.getAncestorByClass'
+    },
+    
+    demoType: [{key: 'default', val: 'baidu.dom.getAncestorByClass'}],
+    'default': {
+        pageConf: {
+            html: '<div id="resultTextarea">获取dom4最近的指定类为red的祖先元素</div><div id="dom1" class="level1 border">dom1<div id="dom2" class="level2  border red">dom2<div id="dom3" class="level3  border">dom3<div id="dom4" class="level4  border">dom4</div></div></div></div>'
+		},
+        formatBtn: {
+            type: 'button',
+            defaultValue: '获取dom4最近的指定类为red的祖先元素',
+            depend: [],
+            event: {
+                eventName: 'onclick',
+                handler: function(){
+					var ancestorId = baidu.dom.getAncestorByClass("dom4", "red").id;
+					baidu.dom.g("resultTextarea").innerHTML='获取dom4最近的指定类为red的祖先元素：'+ancestorId+"<br>";
+                }
+            }
+        }
+    },
+    
+    groups: {
+        'default': [['formatBtn']]
+    }
+};
---
+++ src/demo/baidu_dom_getAttr/conf.js
@@ -0,0 +1,28 @@
+var conf = {
+    clazz: {
+        type: 'method',
+        'method': 'baidu.dom.getAttr'
+    },
+    
+    demoType: [{key: 'default', val: 'baidu.dom.getAttr'}],
+    'default': {
+        pageConf: {
+            html: '<div id="resultTextarea">获取目标元素的value属性</div><input id="test" type="text" size="30" value="被测试的元素" >'
+		},
+        formatBtn: {
+            type: 'button',
+            defaultValue: '获取目标元素的value属性',
+            depend: [],
+            event: {
+                eventName: 'onclick',
+                handler: function(){
+					baidu.dom.g("resultTextarea").innerHTML='获取目标元素的value属性：'+baidu.dom.getAttr("test", "value")+"<br>";
+                }
+            }
+        }
+    },
+    
+    groups: {
+        'default': [['formatBtn']]
+    }
+};
