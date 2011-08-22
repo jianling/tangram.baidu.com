e23b5381dbe67288c15fc1bf84ef9aa54feb34a7
jianling <zhaochengyang@baidu.com>
2011-08-22 16:26:05 +0800

    update demo

--- src/demo/baidu_lang_eventCenter/conf.js
+++ src/demo/baidu_lang_eventCenter/conf.js
@@ -7,22 +7,10 @@ var conf = {
     demoType: [{key: 'default', val: 'baidu.lang.eventCenter'}],
     'default': {
         pageConf: {
-            html: '<div id="resultArea"><button id="button">暂未添加事件的按钮，</button></div>',
-            jsCode: 'function myclick(){ alert("this is my test click"); } baidu.lang.eventCenter.addEventListener("myclick",myclick);'
-        },
-        btn1: {
-            type: 'button',
-            defaultValue: '点击执行代码',
-            isMain: true,
-            event: {
-            	eventName: 'onclick',
-            	handler: function(){
-						baidu.lang.eventCenter.dispatchEvent("myclick");
-            	}
-            }
+            html: '<div id="resultArea">baidu.lang.eventCenter是baidu.lang.Class的单例实例</div>'
         }
     },
     groups: {
-        'default': [['btn1']]
+        'default': []
     }
 };  
