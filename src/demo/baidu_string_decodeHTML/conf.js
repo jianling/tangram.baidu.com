40deeab87460684db45136dd31fe3bf3b4a423f8
linlingyu <linlingyu@baidu.com>
2011-08-23 11:46:20 +0800

    demo

--- src/demo/baidu_string_decodeHTML/conf.js
+++ src/demo/baidu_string_decodeHTML/conf.js
@@ -7,33 +7,26 @@ var conf = {
     demoType: [{key: 'default', val: 'baidu.string.decodeHTML'}],
     'default': {
         pageConf: {
-            html: '<div id="resultArea">解码后返回：</div>',
-            jsCode: 'T.g("dateInput").value = "&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;"'
-        },
-        dateInput: {
-        	label: '输入实体字符：',
-            type: 'text',
-            defaultValue: '',
-            size: 21
+            html: '<div id="resultArea">解码后返回：</div>'
         },
         btn1: {
             type: 'button',
             defaultValue: '实体符号转义回HTML字符',
-            depend: ['dateInput'],
             isMain: true,
             event: {
             	eventName: 'onclick',
-            	handler: function(arg0){
+            	handler: function(){
+            	    var str = '&lt;span class=&quot;test&quot;&gt;test&lt;/span&gt;';
             		var input = document.createElement('input');
             		input.type = "text";
             		input.size = 30;
-            		input.value = baidu.string.decodeHTML(arg0);
+            		input.value = baidu.string.decodeHTML(str);
             		T.g('resultArea').appendChild(input);
             	}
             }
         }
     },
     groups: {
-        'default': [['dateInput'], ['btn1']]
+        'default': [['btn1']]
     }
 };
